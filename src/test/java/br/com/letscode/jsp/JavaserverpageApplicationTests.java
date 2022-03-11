6de9d81cc9b54aa6e3362c9c1cbf4ee6ade733ec
taylorsantosoliveira@hotmail.com <Suco071952804$>
2022-03-10 21:25:15 -0300

    Estrutura inicial.

---
+++ src/test/java/br/com/letscode/jsp/JavaserverpageApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.jsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServerPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
